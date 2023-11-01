31713ff16ae915131c3fe6e748a599a17029a435
barasamichael <ourdigitaltimes@gmail.com>
2023-11-02 02:48:58 +0300

    Implements API Blueprint

---
+++ app/api/views.py
@@ -0,0 +1,31 @@
+import os
+import flask
+import json
+import secrets
+import requests
+from urllib.parse import urlencode
+from flask_login import current_user, login_required, login_user
+from . import api
+from .forms import (RegisterUserForm) 
+from .. import db
+from ..models import User
+
+
+@api.route("/register", methods = ["POST"])
+def register_user():
+    form = RegisterUserForm(request.form)
+    if form.validate_on_submit():
+        user = User(
+                username = form.username.data,
+                password = form.password.data,
+                emailAddress = form.email.data,
+                phoneNumber = form.phone_number.data
+                )
+        db.session.add(user)
+        db.session.commit()
+
+        login_user(user)
+        return jsonify({"message": "User registered successfully"}), 201
+
+    else:
+        return jsonify({"message": "Invalid form data"}), 400
